506a8effded1abb33c88a66164663c654d115228
Kaua Marangoni <kauamarangonitj@gmail.com>
2021-08-27 21:26:42 -0300

    Added option to change theme color

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
+const sidenav = document.querySelector( "nav.sidenav")
+const mainContent = document.querySelector( "main.main-content") 
+const openSidenav = document.querySelector("span.open-sidenav") 
+const closeSidenav = document.querySelector("span.close-sidenav")
+const headerH1 = document.querySelector(".wrapper header h1")
+
+openSidenav.onclick = () => {
+    // sidenav.style.width = "150px"
+    sidenav.style.right = "25px"
+    mainContent.style.marginRight = "-50px"
+}
+    
+closeSidenav.onclick = () => {
+    sidenav.style.right = "-185px"
+    mainContent.style.marginRight = "0"
+}
+
+if (window.matchMedia("(max-width: 335px)").matches) {
+    headerH1.innerHTML = `Lista de <br> Tarefas`
+}
+
 const inputBox = document.querySelector(".inputField input")
 const addBtn = document.querySelector(".inputField button")
 const todoList = document.querySelector(".todoList")
 const deleteAllBtn = document.querySelector(".footer button")
+const wrapper = document.querySelector(".wrapper")
+const body = document.querySelector("body")
+
+const themeBlue = document.querySelector(".wrapper .theme-blue")
+const themePurple = document.querySelector(".wrapper .theme-purple")
+
+themeBlue.onclick = () => {
+    inputBox.style.border = "1px solid #2274A5"
+    addBtn.style.backgroundColor = "#2274A5"
+    deleteAllBtn.style.backgroundColor = "#2274A5"
+    body.style.background = "linear-gradient(to bottom, #68EACC 0%, #2274A5 100%)"
+
+    if (window.matchMedia("(max-width: 445px)").matches) {
+        wrapper.style.border = "5px solid #2274A5"
+    }
+}
+
+themePurple.onclick = () => {
+    inputBox.style.border = "1px solid #8E49E8"
+    addBtn.style.backgroundColor = "#8E49E8"
+    deleteAllBtn.style.backgroundColor = "#8E49E8"
+    body.style.background = "linear-gradient(to bottom, #e2dfe6 0%, #8E49E8 100%)"
+
+    if (window.matchMedia("(max-width: 445px)").matches) {
+        wrapper.style.border = "5px solid #8E49E8"
+    }
+}
 
 if (window.matchMedia("(max-width: 249px)").matches) {
-    inputBox.setAttribute("maxlength", "20");
+    inputBox.setAttribute("maxlength", "20")
 }
 
 if (window.matchMedia("(max-width: 290px)").matches) {
-    inputBox.setAttribute("placeholder", "Digite uma tarefa");
+    inputBox.setAttribute("placeholder", "Digite uma tarefa")
     inputBox.style.paddingLeft = "5px"
 }
 
